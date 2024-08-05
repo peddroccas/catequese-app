a9a58b42d5a4eb661f02155d74e4dbbc13eb40bd
Pedro Bernardes <pedroabernardes11@gmail.com>
2024-08-05 10:56:07 -0300

    feat: create select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,26 @@
+import { Select, SelectItem } from '@nextui-org/react'
+import { ChangeEvent } from 'react'
+
+interface SelectProps {
+  value: string
+  label: string
+  options: string[]
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void
+}
+
+export default function BasicSelect({
+  options,
+  onChange,
+  value,
+  label,
+}: SelectProps) {
+  return (
+    <Select label={label} value={value} onChange={onChange}>
+      {options.map((option) => (
+        <SelectItem key={option} value={option}>
+          {option}
+        </SelectItem>
+      ))}
+    </Select>
+  )
+}
--- src/pages/payment/Payment.tsx
+++ src/pages/payment/Payment.tsx
@@ -1,18 +1,34 @@
 import { Autocomplete, AutocompleteItem, Input } from '@nextui-org/react'
+import { useState } from 'react'
+import Select from '../../components/Select'
 
 export function Payment() {
+  const [segment, setSegment] = useState<string>('')
+  const [catechizing, setCatechizing] = useState<string>('')
+
   return (
     <div className="mt-8 flex w-full flex-col items-center justify-center">
       <h1 className="text-2xl">Pagamento do carnê</h1>
       <form className="mt-8 flex flex-col gap-4 rounded-xl bg-blue-950 p-4">
-        <Autocomplete label="Turma">
-          {[1, 2, 3].map((item) => (
+        <Select
+          label="Segmento"
+          value={segment}
+          options={['1° Eucaristia', 'Crisma', 'Catequese de Adultos']}
+          onChange={(e) => setSegment(e.target.value)}
+        />
+
+        <Autocomplete
+          label="Catequizando"
+          value={catechizing}
+          onChange={(e) => setCatechizing(e.target.value)}
+        >
+          {['1', '2', '3'].map((item) => (
             <AutocompleteItem key={item} value={item}>
               {item}
             </AutocompleteItem>
           ))}
         </Autocomplete>
-        <Input />
+
         <Input />
         <Input />
         <Input />
