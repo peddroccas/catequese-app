a5e2091bd21d27487b7e65104ed374a6a6d56177
Pedro Antunes <pedro@ctir.gov.br>
2024-09-06 14:11:52 -0300

    chore: remove dotenv

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios'
-import 'dotenv'
 
 export const api = axios.create({
-  baseURL: process.env.DATABASE_URL, // https://catequese-api.onrender.com
+  baseURL: import.meta.env.VITE_API_URL, // https://catequese-api.onrender.com
   headers: {
     Accept: 'application/json, text/plain, */*',
     'Content-Type': 'application/json',
