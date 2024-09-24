105da13c18f3b20423b52f5cfff89964a6810242
Pedro Antunes <pedroabernardes11@gmail.com>
2024-09-24 13:43:53 -0300

    feat: add column to catechizings table

--- src/pages/catechizings/components/CatechizingsTable.tsx
+++ src/pages/catechizings/components/CatechizingsTable.tsx
@@ -9,12 +9,15 @@ import {
 } from '@nextui-org/react'
 import { Check, X } from '@phosphor-icons/react'
 import ToolBar from './ToolBar'
+import { useContext } from 'react'
+import { ClassroomContext } from '@/contexts/ClassroomContext'
 
 interface CatechizingsTableProps {
   catechizings: catechizing[]
 }
 
 export function CatechizingsTable({ catechizings }: CatechizingsTableProps) {
+  const { classrooms } = useContext(ClassroomContext)
   return (
     <Table
       aria-label="Catequizandos"
@@ -28,6 +31,7 @@ export function CatechizingsTable({ catechizings }: CatechizingsTableProps) {
         <TableColumn align="start" className="flex-1">
           NOME
         </TableColumn>
+        <TableColumn align="center">TURMA</TableColumn>
         <TableColumn align="center">CARNÊ</TableColumn>
         <TableColumn align="center">BATISMO</TableColumn>
         <TableColumn align="center">1° EUCARISTIA</TableColumn>
@@ -36,44 +40,50 @@ export function CatechizingsTable({ catechizings }: CatechizingsTableProps) {
         </TableColumn>
       </TableHeader>
       <TableBody>
-        {catechizings.map((catechizing) => (
-          <TableRow key={catechizing.id}>
-            <TableCell>{catechizing.name}</TableCell>
-            <TableCell>
-              <div className="flex justify-center text-red-500">
-                {catechizing.payments![0].toBePaid ? (
-                  catechizing.payments![0].toBePaid.toLocaleString('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL',
-                  })
-                ) : (
-                  <Check size={20} className="text-green-500" />
-                )}
-              </div>
-            </TableCell>
-            <TableCell>
-              <div className="flex justify-center">
-                {catechizing.hasReceivedBaptism ? (
-                  <Check size={20} className="text-green-500" />
-                ) : (
-                  <X size={20} className="text-red-500" />
-                )}
-              </div>
-            </TableCell>
-            <TableCell>
-              <div className="flex justify-center">
-                {catechizing.hasReceivedEucharist ? (
-                  <Check size={20} className="text-green-500" />
-                ) : (
-                  <X size={20} className="text-red-500" />
-                )}
-              </div>
-            </TableCell>
-            <TableCell>
-              <ToolBar catechizing={catechizing} />
-            </TableCell>
-          </TableRow>
-        ))}
+        {catechizings.map((catechizing) => {
+          const classroom = classrooms.find(
+            (classroom) => classroom.id === catechizing.classroomId,
+          )
+          return (
+            <TableRow key={catechizing.id}>
+              <TableCell>{catechizing.name}</TableCell>
+              <TableCell>{classroom?.name}</TableCell>
+              <TableCell>
+                <div className="flex justify-center text-red-500">
+                  {catechizing.payments![0].toBePaid ? (
+                    catechizing.payments![0].toBePaid.toLocaleString('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    })
+                  ) : (
+                    <Check size={20} className="text-green-500" />
+                  )}
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex justify-center">
+                  {catechizing.hasReceivedBaptism ? (
+                    <Check size={20} className="text-green-500" />
+                  ) : (
+                    <X size={20} className="text-red-500" />
+                  )}
+                </div>
+              </TableCell>
+              <TableCell>
+                <div className="flex justify-center">
+                  {catechizing.hasReceivedEucharist ? (
+                    <Check size={20} className="text-green-500" />
+                  ) : (
+                    <X size={20} className="text-red-500" />
+                  )}
+                </div>
+              </TableCell>
+              <TableCell>
+                <ToolBar catechizing={catechizing} />
+              </TableCell>
+            </TableRow>
+          )
+        })}
       </TableBody>
     </Table>
   )
